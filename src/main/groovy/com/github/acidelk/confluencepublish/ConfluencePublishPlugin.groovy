cafc4ef6ec4aa1b8bb58f78895454d9afd119aaf
Anton Fedosov <fed.guman@gmail.com>
2019-03-05 23:10:13 +0300

    initial plugin

---
+++ src/main/groovy/com/github/acidelk/confluencepublish/ConfluencePublishPlugin.groovy
@@ -0,0 +1,14 @@
+package com.github.acidelk.confluencepublish
+
+import groovy.transform.CompileStatic
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+@CompileStatic
+class ConfluencePublishPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+}
